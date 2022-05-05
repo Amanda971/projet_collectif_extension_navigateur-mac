3c883c3794e672815205b71b5cab5796c0866b40
clotildebeckerlamoureux <clobecker77@gmail.com>
2022-05-05 09:55:53 +0200

    essai solution boucle

--- extension/wordcount.js
+++ extension/wordcount.js
@@ -1,9 +1,14 @@
-let htmlpage = document.documentElement.innerText
-console.log(htmlpage)
-let count = htmlpage.split(/(?:"\r\n"| )+/)
-console.log(count)
+let paraNumber = document.getElementsByTagName("p")
+console.log(paraNumber)
+for (i=0; i < paraNumber.length; i++) {
+    let htmlpage = paraNumber.innerText
+    console.log(htmlpage)
+}
+// console.log(htmlpage)
+// let count = htmlpage.split(/(?:"\r\n"| )+/)
+// console.log(count)
 
 
 wordsByMinute = 250
-readingSpeed = 
-pageReadingSpeed 
+readingSpeed = 0
+pageReadingSpeed = 0
