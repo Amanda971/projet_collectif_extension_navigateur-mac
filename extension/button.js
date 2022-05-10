0d141e0e9fefbd366da86c5c019fd610d450a1de
clotildebeckerlamoureux <clobecker77@gmail.com>
2022-05-10 16:27:49 +0200

    CSS affichage stockage lien ok

--- extension/button.js
+++ extension/button.js
@@ -3,8 +3,6 @@ let button = document.getElementById("saveUrl")
 button.addEventListener('click', function() {
     console.log("coucou")
 
-    let allURLS = []
-
     chrome.tabs.query({'active': true, 'lastFocusedWindow': true, 'currentWindow': true}, function (tabs) {
         var url = tabs[0].url;
         console.log(url);
@@ -34,8 +32,19 @@ button.addEventListener('click', function() {
         const mergedLists = [...currentLists,...[{urls : savedUrl}]];
         console.log(mergedLists);
         chrome.storage.sync.set({urlLists: mergedLists})
-
     })
     }
 })
 
+    chrome.storage.sync.get(["urlLists"], (obj) => {
+        console.log(obj.urlLists)
+        if(obj.urlLists.length) {
+            for (i=0; i <obj.urlLists.length; i++) {
+                let newLink = document.createElement("a")
+                newLink.setAttribute("href", obj.urlLists[i]["urls"])
+                newLink.setAttribute("target", "_blank")
+                newLink.innerHTML = obj.urlLists[i]["urls"] + "<br />"
+                document.getElementById("myUrl").appendChild(newLink)
+            }
+        }
+    })
